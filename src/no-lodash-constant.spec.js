4d7239505bcf2e016a0a261bac00b174f2590b3e
warren vosper <wvosper@govspend.com>
2021-12-08 10:10:44 -0600

    Update rules and packages
    This commit:
      - Adds missing lint related packages.
      - Copies over rules from eslint-config-smartprocure to .eslintrc for consistency in this repo.
      - Removes prettier rules in favor of equivalent eslint rules.
      - Applies fixes using the new rules and packages.

--- src/no-lodash-constant.spec.js
+++ src/no-lodash-constant.spec.js
@@ -1,47 +1,47 @@
 /* eslint-env jest */
-let _ = require('lodash/fp')
+let _ = require("lodash/fp");
 
-let { RuleTester } = require('eslint')
-let rule = require('./no-lodash-constant')
+let { RuleTester } = require("eslint");
+let rule = require("./no-lodash-constant");
 
 RuleTester.setDefaultConfig({
   parserOptions: {
-    ecmaVersion: 6
-  }
-})
+    ecmaVersion: 6,
+  },
+});
 
-let ruleTester = new RuleTester()
+let ruleTester = new RuleTester();
 
 let validCode = [
   "() => { return ''; }",
   "() => ''",
   "(function() { return ''; })",
-  "function foo() { return ''; }"
-]
+  "function foo() { return ''; }",
+];
 let invalidCode = [
   { input: "_.constant('')", output: "() => ''" },
   { input: "_.constant('{}')", output: "() => '{}'" },
-  { input: '_.constant(1)', output: '() => 1' },
-  { input: '_.constant(true)', output: '() => true' },
-  { input: '_.constant({})', output: '() => ({})' },
+  { input: "_.constant(1)", output: "() => 1" },
+  { input: "_.constant(true)", output: "() => true" },
+  { input: "_.constant({})", output: "() => ({})" },
   {
-    input: 'var foo = {}; _.constant(foo)',
-    output: 'var foo = {}; () => foo'
-  }
-]
+    input: "var foo = {}; _.constant(foo)",
+    output: "var foo = {}; () => foo",
+  },
+];
 
-ruleTester.run('no-lodash-constant', rule, {
+ruleTester.run("no-lodash-constant", rule, {
   valid: _.map((code) => ({ code }), validCode),
   invalid: _.map(
     ({ input, output }) => ({
       code: input,
       errors: [
         {
-          message: 'Do not use _.constant'
-        }
+          message: "Do not use _.constant",
+        },
       ],
-      output
+      output,
     }),
     invalidCode
-  )
-})
+  ),
+});
